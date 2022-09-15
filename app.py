8d6cd6863a06505a463d2e6f9ebd26aa4915b905
Nguyễn Thị Kim Anh <79450471+kimanh24022001@users.noreply.github.com>
2022-09-16 01:38:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ def home():
         context = request.form.get("para")
         question = request.form.get("qa")
         # Make predictions with the model
-        tokenizer = AutoTokenizer.from_pretrained("best_model")
-        model = AutoModelForQuestionAnswering.from_pretrained("best_model")
+        tokenizer = AutoTokenizer.from_pretrained("https://drive.google.com/drive/u/1/folders/1xKrDyY5DNGoRhSOv50qkbk6r0MbQZuHi")
+        model = AutoModelForQuestionAnswering.from_pretrained("https://drive.google.com/drive/u/1/folders/1xKrDyY5DNGoRhSOv50qkbk6r0MbQZuHi")
         question_answerer = pipeline("question-answering", model = model, tokenizer= tokenizer)
         return render_template('index.html',qa=question,para=context,message=question_answerer(question=question, context = context)['answer'])
     return render_template('index.html')
