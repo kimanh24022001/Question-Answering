05a873879d2192dbdab0dec3e5c5489beb7bc4a0
Nguyễn Thị Kim Anh <79450471+kimanh24022001@users.noreply.github.com>
2022-09-16 16:56:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ def home():
         context = request.form.get("para")
         question = request.form.get("qa")
         # Make predictions with the model
-        tokenizer = AutoTokenizer.from_pretrained("deepset/roberta-base-squad2")
-        model = TFAutoModelForQuestionAnswering.from_pretrained("deepset/roberta-base-squad2")
+        tokenizer = AutoTokenizer.from_pretrained("distilbert-base-cased-distilled-squad")
+        model = TFAutoModelForQuestionAnswering.from_pretrained("distilbert-base-cased-distilled-squad")
         question_answerer = pipeline("question-answering", model = model, tokenizer= tokenizer)
         return render_template('index.html',qa=question,para=context,message=question_answerer(question=question, context = context)['answer'])
     return render_template('index.html')
