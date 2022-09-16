e3a243f328a0e9ce461a99a6463edc65184e52c7
Nguyễn Thị Kim Anh <79450471+kimanh24022001@users.noreply.github.com>
2022-09-16 17:34:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,29 @@ import PyPDF2
 import os
 from transformers import AutoTokenizer,TFAutoModelForQuestionAnswering,pipeline
 app = Flask(__name__)
-
+app.config['SECRET_KEY']='supersecretkey'
+UPLOAD_FOLDER = 'static'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/',methods=["GET","POST"])
+@app.route('/home',methods=["GET","POST"])
+
 def home():
+    
     if request.method=="POST":
-        context = request.form.get("para")
-        question = request.form.get("qa")
-        # Make predictions with the model
-        tokenizer = AutoTokenizer.from_pretrained("distilbert-base-cased-distilled-squad")
-        model = TFAutoModelForQuestionAnswering.from_pretrained("distilbert-base-cased-distilled-squad")
-        question_answerer = pipeline("question-answering", model = model, tokenizer= tokenizer)
-        return render_template('index.html',qa=question,para=context,message=question_answerer(question=question, context = context)['answer'])
-    return render_template('index.html')
+        f = request.files['File'] 
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+        pdfFileObj = open(os.path.join(app.config['UPLOAD_FOLDER'], f.filename), 'rb')
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        pageObj = pdfReader.getPage(0)
+        extract = pageObj.extractText()
+        if len(str(request.form.get("para")))>4:
+            tokenizer = AutoTokenizer.from_pretrained("distilbert-base-cased-distilled-squad")
+            model = TFAutoModelForQuestionAnswering.from_pretrained("distilbert-base-cased-distilled-squad")
+            question_answerer = pipeline("question-answering", model = model, tokenizer= tokenizer)
+            return render_template('index_document.html',note='See the results. You want it?  ',qa=extract,para=request.form.get("para"),message=question_answerer(question=extract, context = request.form.get("para"))['answer'])
+        else:
+            return render_template('index_document.html',note='Please upload for question',para=extract)
+    return render_template('index_document.html',note='Please upload for paragraph')
 if __name__=="__main__":
     app.run(debug=True)
     
