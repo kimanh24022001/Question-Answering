5c6bbf0d6284c3e9ba8e586ae50178ecddf4c059
Nguyễn Thị Kim Anh <79450471+kimanh24022001@users.noreply.github.com>
2022-09-16 15:56:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template,request, redirect, url_for
 import PyPDF2
 import os
-from transformers import AutoTokenizer,AutoModelForQuestionAnswering,pipeline
+from transformers import TFAutoTokenizer,TFAutoModelForQuestionAnswering,pipeline
 app = Flask(__name__)
 
 @app.route('/',methods=["GET","POST"])
@@ -10,8 +10,8 @@ def home():
         context = request.form.get("para")
         question = request.form.get("qa")
         # Make predictions with the model
-        tokenizer = AutoTokenizer.from_pretrained("deepset/roberta-base-squad2")
-        model = AutoModelForQuestionAnswering.from_pretrained("deepset/roberta-base-squad2")
+        tokenizer = TFAutoTokenizer.from_pretrained("deepset/roberta-base-squad2")
+        model = TFAutoModelForQuestionAnswering.from_pretrained("deepset/roberta-base-squad2")
         question_answerer = pipeline("question-answering", model = model, tokenizer= tokenizer)
         return render_template('index.html',qa=question,para=context,message=question_answerer(question=question, context = context)['answer'])
     return render_template('index.html')
